db42a5d90648b90e4ddd3df5271fbc6ae0cc2dcc
Pal patel <115624654+PalProdution@users.noreply.github.com>
2024-07-31 16:13:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,3 +37,27 @@ def game():
   if pChoice == cChoice: #checking for tie
     print("Its a tie! No one gets a point. ")
   elif (pChoice == "r" and cChoice == "s") or (pChoice == "s" and cChoice == "p") or (pChoice == "p" and cChoice == "r"): #checking if player scores a point
+   print("You won! ")
+    pPoints += 1 #increment the players point by 1
+  else: #checking if computer scores a point
+    print("Aww. I won. ")
+    cPoints += 1 #increment the computer point by 1
+  print()
+ 
+print("Welcome to Rock Paper and Scissors game!!!")
+while True: #creating an infinite loop
+  for i in range(5):#loop that runs 5 times
+    game() #calling the game for every single round
+  print("Good job!\nYour score is:", pPoints, "\nMy score is", cPoints) #displaying the result after 5 rounds
+  print()
+  again = int(input("Press 1 to continue\nPress2 to reset and continue\npress 3 to exit "))
+  if again==1: #this is executed if the user wants to play another 5 rounds
+    continue
+  elif again==2: #this is executed if the user wants to reset the scores and play another 5 rounds
+    pPoints=0
+    cPoints=0
+    continue
+  else: #this is executed if the user wants to quit.
+    break
+     
+print("Ok! Bye!")
